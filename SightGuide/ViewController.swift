7b4e5be09be43624d17c8aa59a3f785828bb1777
hamster <daiyue10@gmail.com>
2023-03-18 08:25:24 +0800

    Initial Commit

---
+++ SightGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SightGuide
+//
+//  Created by FindTheLamp on 2023/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
